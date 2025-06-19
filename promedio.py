99420d256495588790942e8a95b12807a025036d
SYST3M644 <your-email@example.com>
2025-06-18 20:46:04 -0400

    Actualizacion de Promedio

--- promedio.py
+++ promedio.py
@@ -1,3 +1,29 @@
 numeros = [10, 20, 30, 40, 50]
 promedio = sum(numeros) / len(numeros)
 print("El promedio es:", promedio)
+
+
+numeros = [10, 20, 30, 40, 50]
+promedio = sum(numeros) / len(numeros)
+print("El promedio es:", promedio)
+
+
+numeros = [10, 20, 30, 40, 50]
+promedio = sum(numeros) / len(numeros)
+print("El promedio es:", promedio)
+
+
+
+numeros = [10, 20, 30, 40, 50]
+promedio = sum(numeros) / len(numeros)
+print("El promedio es:", promedio)
+
+
+numeros = [10, 50, 30, 70, 50]
+promedio = sum(numeros) / len(numeros)
+print("El promedio es:", promedio)
+
+
+numeros = [10, 50, 30, 70, 50]
+promedio = sum(numeros) / len(numeros)
+print("El promedio es:", promedio)
